722b08415281430829be8573cdf866d5e1ce7c66
Mateusz Urbańczyk <mateurb251@student.polsl.pl>
2023-04-27 23:34:03 +0200

    logowanie + uprawnienia + pobieranie danych z bazy

--- Gym_Aplication/MainWindow.xaml.cs
+++ Gym_Aplication/MainWindow.xaml.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using MySql.Data.MySqlClient; //import
+using System.Security.Cryptography;
+using System.Text;
+
 namespace Gym_Aplication
 {
     public partial class MainWindow : Window
     {
+        //na chwilę obecną bez zabezpieczeń user: test password: Pa$$w0rd
+        private string username = "";
+        private string password = "";
+        private int user_id = 0;
+        private int user_privilege = 0;
+        private string connection_string = "server=polsl.online;" + "uid=test" + ";pwd=Pa$$w0rd" + ";database=Baza_projekt;";
+        MySqlConnection connection_name = new MySqlConnection();
+
         public class ScheduleEntry
         {
             public string Time { get; set; }
@@ -14,6 +25,8 @@ namespace Gym_Aplication
             public string Trainer { get; set; }
             public string Room { get; set; }
         }
+
+
 
         public MainWindow()
         {
@@ -23,50 +36,65 @@ namespace Gym_Aplication
 
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
-            //na chwilę obecną bez zabezpieczeń user: test password: Pa$$w0rd
-            string username = UsernameTextBox.Text;
-            string password = PasswordTextBox.Password;
-            string server= "server=polsl.online;";
-            string connection_string = server += "uid=" + username + ";pwd=" + password + ";database=Baza_projekt;";
 
-            MySqlConnection connection_name = new MySqlConnection();
+            username = UsernameTextBox.Text;
+            password = PasswordTextBox.Password;
+
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+                password = BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+            }
+
             connection_name.ConnectionString = connection_string;
+
             connection_name.Open();
 
-            string querry1= "SELECT * FROM `Karnet`;";
+            string querry1 = "SELECT * FROM `Uzytkownicy` WHERE Nazwa LIKE \"" + username + "\" AND Hasło LIKE \"" + password + "\";";
 
             MySqlCommand commend = new MySqlCommand(querry1, connection_name);
             MySqlDataReader data_from_querry1 = commend.ExecuteReader();
 
+
+
+
             while (data_from_querry1.Read())
             {
-                MessageBox.Show("Należy zużyć do:   " + data_from_querry1["data_warznosci"]);
+                user_id = (int)data_from_querry1["ID"];
+                user_privilege = (int)data_from_querry1["Uprawnienia"];
             }
 
-            //if (username == "admin" && password == "password") 
-            //{
-            //    MessageBox.Show("Logged in successfully.");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Invalid username or password.");
-            //}
+            if (user_id != 0)
+            {
+                MessageBox.Show("Logowanie zakończone pomyślnie. Witaj: " + username + " Twoje uprawnienia to: " + user_privilege);
+                connection_name.Close();
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password.");
+                connection_name.Close();
+            }
+
+
         }
 
         private void PopulateScheduleDataGrid()
         {
             var sampleSchedule = new List<ScheduleEntry>
             {
-                new ScheduleEntry { Time = "8:00-9:00", Activity = "Yoga", Trainer = "Jan Kowalski", Room = "1" },
-                new ScheduleEntry { Time = "10:00-11:00", Activity = "Zumba", Trainer = "Anna Nowak", Room = "2" },
-                new ScheduleEntry { Time = "12:00-13:00", Activity = "Kickboxing", Trainer = "Piotr Wiśniewski", Room = "3" },
-                new ScheduleEntry { Time = "14:00-15:00", Activity = "Spinning", Trainer = "Marta Zielińska", Room = "4" },
+                    new ScheduleEntry { Time = "8:00-9:00", Activity = "Yoga", Trainer = "Jan Kowalski", Room = "1" },
+                    new ScheduleEntry { Time = "10:00-11:00", Activity = "Zumba", Trainer = "Anna Nowak", Room = "2" },
+                    new ScheduleEntry { Time = "12:00-13:00", Activity = "Kickboxing", Trainer = "Piotr Wiśniewski", Room = "3" },
+                    new ScheduleEntry { Time = "14:00-15:00", Activity = "Spinning", Trainer = "Marta Zielińska", Room = "4" },
+
+
             };
 
             ScheduleDataGrid.ItemsSource = sampleSchedule;
         }
 
-        
+
         private void SwitchTab(int tabIndex)
         {
             MainTabControl.SelectedIndex = tabIndex;
@@ -74,8 +102,8 @@ namespace Gym_Aplication
 
         private void ReserveEquipment_Click(object sender, RoutedEventArgs e)
         {
-            
-            bool isEquipmentAvailable = true; 
+
+            bool isEquipmentAvailable = true;
             if (isEquipmentAvailable)
             {
                 MessageBox.Show("Equipment reserved successfully.");
@@ -88,36 +116,36 @@ namespace Gym_Aplication
 
         private void RateTraining_Click(object sender, RoutedEventArgs e)
         {
-            
-            int rating = 4; 
+
+            int rating = 4;
             MessageBox.Show($"Training rated {rating} out of 5.");
         }
 
         private void AddTrainer_Click(object sender, RoutedEventArgs e)
         {
-            
-            string trainerName = "New Trainer"; 
+
+            string trainerName = "New Trainer";
             MessageBox.Show($"Trainer {trainerName} added successfully.");
         }
 
         private void RemoveTrainer_Click(object sender, RoutedEventArgs e)
         {
-            
-            string trainerName = "Trainer to Remove"; 
+
+            string trainerName = "Trainer to Remove";
             MessageBox.Show($"Trainer {trainerName} removed successfully.");
         }
 
         private void ReserveClass_Click(object sender, RoutedEventArgs e)
         {
-            
-            string className = "Yoga"; 
+
+            string className = "Yoga";
             MessageBox.Show($"{className} class reserved successfully.");
         }
 
         private void ReportIssue_Click(object sender, RoutedEventArgs e)
         {
-            
-            string equipmentName = "Treadmill"; 
+
+            string equipmentName = "Treadmill";
             MessageBox.Show($"Issue with {equipmentName} reported successfully.");
         }
 
@@ -187,7 +215,14 @@ namespace Gym_Aplication
 
         private void Wyloguj_Click(object sender, RoutedEventArgs e)
         {
+            user_privilege = 0;
+            user_id = 0;
+            UsernameTextBox.Text = "";
+            PasswordTextBox.Password = "";
             MessageBox.Show("Wylogowywanie...");
+
         }
+
+
     }
 }
