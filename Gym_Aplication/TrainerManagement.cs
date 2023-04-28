c0381f5986bd23f75837f875fe009453b2ecf655
Mateusz Urbańczyk <mateurb251@student.polsl.pl>
2023-04-28 21:55:36 +0200

    -podział na oddzielne pliki z klsamai -dodanie klasy TrainerManagment -rozpoczęcie prac na listą trenerów zaczytaną z bazy

---
+++ Gym_Aplication/TrainerManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Aplication
+{
+    internal class TrainerManagement
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string E_Mail { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string DateOfEmployment { get; set; }
+
+    }
+}
