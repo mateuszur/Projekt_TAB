da853896b4e5757734ed13e1ee3a3a65e0282b3c
Mateusz Urbańczyk <mateurb251@student.polsl.pl>
2023-06-11 22:45:32 +0200

    Dużo zmian -porządki -okodowanie zarządzania trenerami -rozbicie na więcej plików

---
+++ Gym_Aplication/NumericComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Aplication
+{
+    public class NumericComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            if (x == null || y == null)
+            {
+                return 0;
+            }
+
+            if (int.TryParse(x.ToString(), out int xValue) && int.TryParse(y.ToString(), out int yValue))
+            {
+                return xValue.CompareTo(yValue);
+            }
+
+            return 0;
+        }
+    }
+
+}
---
+++ Gym_Aplication/ReverseComparer .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym_Aplication
+{
+    public class ReverseComparer : IComparer
+    {
+        private readonly IComparer _baseComparer;
+
+        public ReverseComparer(IComparer baseComparer)
+        {
+            _baseComparer = baseComparer;
+        }
+
+        public int Compare(object x, object y)
+        {
+            return _baseComparer.Compare(y, x); // Odwrócenie kolejności argumentów
+        }
+    }
+
+}
