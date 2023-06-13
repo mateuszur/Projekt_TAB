e9e021fe6e9b3c481549c4bdf13a52a265afd8d9
Mateusz Urbańczyk <mateurb251@student.polsl.pl>
2023-06-14 00:39:52 +0200

    Poprawka błędów

--- Gym_Aplication/Trainer_Details.cs
+++ Gym_Aplication/Trainer_Details.cs
@@ -3,21 +3,56 @@ using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Drawing;
 using System.Drawing.Printing;
 using System.Windows;
 using System.Windows.Data;
-using System.Windows.Forms;
+
 
 
 namespace Gym_Aplication
 {
     public partial class MainWindow : Window
     {
-      
 
-        // Declare a PrintDocument object named document.
-        private System.Drawing.Printing.PrintDocument document =
-            new System.Drawing.Printing.PrintDocument();
+        //
+
+        //// Declare a string to hold the entire document contents.
+        //private string documentContents = "";
+
+        //// Declare a variable to hold the portion of the document that
+        //// is not printed.
+        //private string stringToPrint = "";
+
+
+        //void PrintDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        //{
+        //    int charactersOnPage = 0;
+        //    int linesPerPage = 0;
+
+        //    // Sets the value of charactersOnPage to the number of characters
+        //    // of stringToPrint that will fit within the bounds of the page.
+        //    e.Graphics.MeasureString(stringToPrint, this.Font,
+        //        e.MarginBounds.Size, StringFormat.GenericTypographic,
+        //        out charactersOnPage, out linesPerPage);
+
+        //    // Draws the string within the bounds of the page.
+        //    e.Graphics.DrawString(stringToPrint, this.Font, Brushes.Black,
+        //    e.MarginBounds, StringFormat.GenericTypographic);
+
+        //    // Remove the portion of the string that has been printed.
+        //    stringToPrint = stringToPrint.Substring(charactersOnPage);
+
+        //    // Check to see if more pages are to be printed.
+        //    e.HasMorePages = (stringToPrint.Length > 0);
+
+        //    // If there are no more pages, reset the string to be printed.
+        //    if (!e.HasMorePages)
+        //        stringToPrint = documentContents;
+       // }
+
+
+        //
 
         private void Zarzadzanie_Click(object sender, RoutedEventArgs e)
 
@@ -141,50 +176,42 @@ namespace Gym_Aplication
             // MessageBox.Show("Ścieżka: " +finalFilePath );
 
 
-            connection_name.Open();
+            //connection_name.Open();
 
-            string querry2 = "SELECT * FROM `Trenerzy` WHERE imie NOT LIKE '';";
+            //string querry2 = "SELECT * FROM `Trenerzy` WHERE imie NOT LIKE '';";
 
-            MySqlCommand commend2 = new MySqlCommand(querry2, connection_name);
-            MySqlDataReader data_from_querry2 = commend2.ExecuteReader();
+            //MySqlCommand commend2 = new MySqlCommand(querry2, connection_name);
+            //MySqlDataReader data_from_querry2 = commend2.ExecuteReader();
 
-            var listOfTrainers = new List<TrainerManagement>();
+            //var listOfTrainers = new List<TrainerManagement>();
 
-            while (data_from_querry2.Read())
-            {
-                TrainerManagement feld = new TrainerManagement
-                {
-                    ID = int.Parse(data_from_querry2["id_trenera"].ToString()),
-                    Name = (data_from_querry2["imie"]).ToString(),
-                    Surname = (data_from_querry2["nazwisko"]).ToString(),
-                    Phone = data_from_querry2["telefon"].ToString(),
-                    E_Mail = data_from_querry2["e-mail"].ToString(),
-                    DateOfBirth = data_from_querry2["data_urodzenia"].ToString(),
-                    Address = data_from_querry2["adres"].ToString(),
-                    DateOfEmployment = data_from_querry2["data_zatrudnienia"].ToString()
-                };
+            //while (data_from_querry2.Read())
+            //{
+            //    TrainerManagement feld = new TrainerManagement
+            //    {
+            //        ID = int.Parse(data_from_querry2["id_trenera"].ToString()),
+            //        Name = (data_from_querry2["imie"]).ToString(),
+            //        Surname = (data_from_querry2["nazwisko"]).ToString(),
+            //        Phone = data_from_querry2["telefon"].ToString(),
+            //        E_Mail = data_from_querry2["e-mail"].ToString(),
+            //        DateOfBirth = data_from_querry2["data_urodzenia"].ToString(),
+            //        Address = data_from_querry2["adres"].ToString(),
+            //        DateOfEmployment = data_from_querry2["data_zatrudnienia"].ToString()
+            //    };
 
-                listOfTrainers.Add(feld);
-            }
-            connection_name.Close();
+            //    listOfTrainers.Add(feld);
+            //}
+            //connection_name.Close();
 
+            //string docName = "testPage.txt";
+            //string docPath = @"C:\";
+            //string fullPath = System.IO.Path.Combine(docPath, docName);
+            //printDocument1.DocumentName = docName;
+            //stringToPrint = System.IO.File.ReadAllText(fullPath);
 
+            //printPreviewDialog1.Document = printDocument1;
 
-            PrintDocument printDocument = new PrintDocument();
-            printDocument.PrintPage += (sender, e) =>
-            {
-                e.Graphics.DrawString("Przykładowy tekst do wydruku", new Font("Arial", 12), Brushes.Black, 100, 100);
-            };
-
-            PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
-            printPreviewDialog.Document = printDocument;
-
-            // Opcjonalnie można dostosować inne ustawienia podglądu wydruku
-            // np. Zoom, Marginesy, itp.
-
-            printPreviewDialog.ShowDialog();
-
-
+            //printPreviewDialog1.ShowDialog();
 
         }
             //using (var writer = new StreamWriter(path,false, Encoding.UTF8))
@@ -204,4 +231,3 @@ namespace Gym_Aplication
  
         
     }
-}
