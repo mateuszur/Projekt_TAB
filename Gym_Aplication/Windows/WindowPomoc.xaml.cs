59576d00df4246fd8d98b3b3f694217e80a072e7
majawis193 <72254280+majawis193@users.noreply.github.com>
2023-06-18 15:46:50 +0200

    Help

--- Gym_Aplication/Windows/WindowPomoc.xaml.cs
+++ Gym_Aplication/Windows/WindowPomoc.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Input;
 
 namespace Gym_Aplication
 {
@@ -31,10 +31,94 @@ namespace Gym_Aplication
             {
                 FAQs = new List<FAQ>
                 {
+                    new FAQ { Question = "Jak mogę zmienić hasło do mojego konta?", Answer = "Aby mienić hasło należy skontaktować się z administartorem." },
+                    new FAQ { Question = "Jak korzystać z aplikacji?", Answer = "Oto jak korzystać z aplikacji..." },
+                    new FAQ { Question = "Jak korzystać z aplikacji?", Answer = "Oto jak korzystać z aplikacji..." },
                     new FAQ { Question = "Jak korzystać z aplikacji?", Answer = "Oto jak korzystać z aplikacji..." },
                     // Add more FAQs here...
                 };
             }
         }
+
+        public void CollapseContent () 
+        {
+            FAQListBox.Visibility = Visibility.Collapsed;
+
+        }
+
+        public void ShowFAQ(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Najczęściej zadawane pytania (FAQ)";
+        }
+
+        public void ShowClassSchedule(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Harmonogram zajęć";
+        }
+
+
+        public void ShowPersonalSchedule(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Harmonogram indywidualny";
+        }
+
+
+
+        public void ShowTrainersMenage(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Zarządzanie trenerami";
+        }
+
+
+
+        public void ShowMembersMenage(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Zarządzanie członkami";
+        }
+
+
+
+        public void ShowArticles(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Artykuły i porady";
+        }
+
+
+        public void ShowStatistic(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Statystyki i analizy";
+        }
+
+        public void ShowReports(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Raportowanie awarii";
+        }
+
+        public void ShowInfo(object sender, MouseButtonEventArgs arg)
+        {
+            CollapseContent();
+            FAQListBox.Visibility = Visibility.Visible;
+            contentLabel.Content = "Informacje";
+        }
+
+
+
     }
+
 }
